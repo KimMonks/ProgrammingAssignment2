3189ac80a71160007f61b613e03d7075bc6b09c7
KimMonks <kim.monks@systemic.com.au>
2015-02-20 22:21:53 +0800

    Initial

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A function is provided to get and set a matrix and to get and set the "solve" the matrixPut comments here that give an overall description of what your
+## A second function is prvided that will only "slve " the matrix if it is not already solved and cached
 
-## Write a short comment describing this function
+## Provide the matrix handlers
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
+## return the solve matrix by function call or from cache if available
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
